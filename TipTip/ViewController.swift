614e9a63d0bba5bd5b58f3b42ed2a6c45083572d
Chase McElroy <ichasem@gmail.com>
2016-08-08 11:33:13 -0700

    Added Story board and tip calculations

--- TipTip/ViewController.swift
+++ TipTip/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipSegmentedControll: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipTitleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tipTitleLabel.text = NSLocalizedString("MainPage.TipTitleLabel", comment: "This is the Label talking about the tip")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func textFieldChanged(sender: AnyObject) {
+        calculateTotal()
     }
 
-
+    @IBAction func valueChanged(sender: UISegmentedControl) {
+        calculateTotal()
+    }
+    
+    func calculateTotal() {
+        if let bill : Double = Double(self.billTextField.text!) {
+            var tipPercentage : Double
+            
+            if self.tipSegmentedControll.selectedSegmentIndex == 0 {
+                tipPercentage = 0.10
+            }
+            if self.tipSegmentedControll.selectedSegmentIndex == 1 {
+                tipPercentage = 0.15
+            } else {
+                tipPercentage = 0.20
+            }
+            
+            let tip = bill * tipPercentage
+            let total = tip + bill
+            
+            self.tipLabel.text = "$\(tip)"
+            self.totalLabel.text = "$\(total)"
+        }
+    }
 }
 
